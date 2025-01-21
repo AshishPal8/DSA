b030faa4279e12591cf8f477ace2f69b843bb3c8
Ashish Pal <ashish81301669@gmail.com>
2025-01-21 19:45:44 +0530

    selection, bubble, insertion sort algo

---
+++ sorting/sort.cpp
@@ -0,0 +1,80 @@
+
+#include <iostream>
+using namespace std;
+
+// finds the lowest value in an array and moves it to the front of the array
+// TC worst and average case = O(n^2), best = O(n^2)
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int mini = i;
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[mini])
+            {
+                mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+//  loops through every value in the array, comparing it to the value next to it.
+// TC worst and average case = O(n^2), best = O(n)
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int didSwap = 0;
+        for (int j = 0; j <= n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0)
+        {
+            break;
+        }
+        cout << "Runs\n";
+    }
+}
+
+// iteratively inserting each element of an unsorted list into its correct position
+// TC worst and average case = O(n^2), best = O(n)
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    insertion_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
