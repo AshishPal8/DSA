ccff077ede8689a61197f0e7e5a6edfe1b74d88a
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2025-10-10 13:28:46 +0000

    started stack

--- 9.stack/implementation.cpp
+++ 9.stack/implementation.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+struct Node {
+    int val;
+    Node* next;
+}
+
 class Stack {
     int size;
     int *arr;
@@ -31,8 +36,43 @@ class Stack {
         }
 };
 
+class StackLinkedlist {
+    Node* top = NULL;
+    int size = 0;
+
+    public: 
+       void push(int x){
+            newNode = new Node(x);
+            newNode->next = top;
+            top = newNode;
+            size++;
+        }
+
+        int pop(){
+            if(!top) return NULL;
+            int val = top->val;
+            temp = top;
+            top = top->next;
+            free(temp);
+            size--;
+            return val;
+        }
+
+        int top(){
+            if(!top) return NULL;
+
+            return top->val;
+        }
+
+        int size(){
+            return size;
+        }
+}
+
+
+
 int main(){
-    Stack s;
+    StackLinkedlist s;
 
     s.push(6);
     s.push(3);
@@ -40,10 +80,10 @@ int main(){
     s.push(71);
     s.push(6);
 
-    cout << "Top of stack is before deleting any element " << s.Top() << endl;
-    cout << "Size of stack is before deleting any element " << s.Size() << endl;
+    cout << "Top of stack is before deleting any element " << s.top() << endl;
+    cout << "Size of stack is before deleting any element " << s.size() << endl;
     cout << "Element deleted: " << s.pop() << endl;
-     cout << "Size of stack after deleting an element " << s.Size() << endl;
+     cout << "Size of stack after deleting an element " << s.size() << endl;
 //   cout << "Top of stack after deleting an element " << s.Top() << endl;
 //   return 0;
 }
