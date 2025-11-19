e8233cc3a6fde8310fc5ff903edb3d56db7108b0
AshishPal8 <ashish81301669@gmail.com>
2025-11-19 05:50:18 +0530

    add longest substring

--- 11.pointer_slidingWindow/longestSubstring.cpp
+++ 11.pointer_slidingWindow/longestSubstring.cpp
@@ -4,16 +4,21 @@
 
 using namespace std;
 
-int longestSubstringBrute(string str){
-    if(str.size() == 0) return 0;
+int longestSubstringBrute(string str)
+{
+    if (str.size() == 0)
+        return 0;
     int maxlen = INT_MIN;
 
-    for(int i = 0; i < str.length(); i++){
+    for (int i = 0; i < str.length(); i++)
+    {
         unordered_set<int> set;
 
-        for(int j = i; j <= str.length(); j++){
-            if(set.find(str[j]) != set.end()){
-                maxlen = max(maxlen, j -i);
+        for (int j = i; j <= str.length(); j++)
+        {
+            if (set.find(str[j]) != set.end())
+            {
+                maxlen = max(maxlen, j - i);
                 break;
             }
             set.insert(str[j]);
@@ -23,27 +28,33 @@ int longestSubstringBrute(string str){
     return maxlen;
 }
 
-int longestSubstringBrute1(string str){
-    if(str.size() == 0) return 0;
+int longestSubstringBrute1(string str)
+{
+    if (str.size() == 0)
+        return 0;
     int maxlen = INT_MIN;
     unordered_set<int> set;
     int l = 0;
 
-    for(int r = 0;r < str.length(); r++){
-        if(set.find(str[r]) != set.end()){
-            while(l < r && set.find(str[r]) != set.end()){
+    for (int r = 0; r < str.length(); r++)
+    {
+        if (set.find(str[r]) != set.end())
+        {
+            while (l < r && set.find(str[r]) != set.end())
+            {
                 set.erase(str[l]);
                 l++;
             }
         }
         set.insert(str[r]);
-        maxlen = max(maxlen, r-l+1);
+        maxlen = max(maxlen, r - l + 1);
     }
 
     return maxlen;
 }
 
-int main (){
+int main()
+{
     string s = "abcaabcda";
     int result = longestSubstringBrute1(s);
 
